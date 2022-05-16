68a84584419119b7b61056273de9d11850035720
Nicholas <appledev.booker@gmail.com>
2022-05-16 18:45:52 +0900

    #10 feat: 플로팅 버튼까지 구현

--- Project-ClonePassOrder/StoreDetail/StoreDetailViewController.swift
+++ Project-ClonePassOrder/StoreDetail/StoreDetailViewController.swift
@@ -13,82 +13,67 @@ final class StoreDetailViewController: UIViewController {
         let scrollView = UIScrollView()
         return scrollView
     }()
-    
-    let labelOne: UILabel = {
-      let label = UILabel()
-      label.text = "Scroll Top"
-      label.backgroundColor = .red
-      label.translatesAutoresizingMaskIntoConstraints = false
-      return label
+    private let floatingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    private let floatingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("가져 갈게요\n메뉴 보기", for: .normal)
+        button.titleLabel?.lineBreakMode = .byWordWrapping
+        button.titleLabel?.textAlignment = .center
+        button.backgroundColor = #colorLiteral(red: 1, green: 0.4730066061, blue: 0.2864735723, alpha: 1)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    private let storeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .gray
+        return imageView
+    }()
+    private let storeInfoView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private let storeName: UILabel = {
+        let label = UILabel()
+        label.text = "빽따방!"
+        label.font = .preferredFont(forTextStyle: .title2)
+        return label
+    }()
+    private let storeDescription: UILabel = {
+        let label = UILabel()
+        label.text = """
+                        백종원이 하는 백다방!
+                        커피가 맛있어요👍🏻
+                        """
+        label.textColor = .lightGray
+        label.numberOfLines = 0
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
     }()
 
-    let labelTwo: UILabel = {
-      let label = UILabel()
-      label.text = "Scroll Bottom"
-      label.backgroundColor = .red
-      label.translatesAutoresizingMaskIntoConstraints = false
-      return label
-    }()
-//    private let storeImageView: UIImageView = {
-//        let imageView = UIImageView()
-//        imageView.backgroundColor = .gray
-//        return imageView
-//    }()
-//    private let storeInfoView: UIView = {
-//        let view = UIView()
-//        view.backgroundColor = .yellow
-//        return view
-//    }()
-//    private let storeName: UILabel = {
-//        let label = UILabel()
-//        label.text = "빽따방!"
-//        label.font = .preferredFont(forTextStyle: .title2)
-//        return label
-//    }()
-//    private let storeDescription: UITextView = {
-//        let textView = UITextView()
-//        textView.text = """
-//                        안녕하세요!
-//                        빽다방입니다.
-//                        ㅇㅇ
-//                        ㅇㄸ
-//                        ㅇ3ㄷ
-//                        ㅎㄷㄱㅎㄷ
-//
-//                        ㄷㄱㅎㄷㄱ
-//                        ㅎ
-//                        ㄷㄱㅎ
-//                        ㄷㄱㅎㄷㄱ
-//                        """
-//        textView.textColor = .lightGray
-//        textView.backgroundColor = .clear
-//        textView.isEditable = false
-//        textView.isSelectable = false
-//        textView.isScrollEnabled = false
-//        textView.font = .preferredFont(forTextStyle: .body)
-//        return textView
-//    }()
-    
-    
     // MARK: - viewLifeCycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         makeUI()
     }
-    
+
     private func makeUI() {
         view.backgroundColor = .tertiarySystemGroupedBackground
         
         view.addSubview(scrollView)
+        view.addSubview(floatingView)
         
-//        scrollView.addSubview(storeImageView)
-//        scrollView.addSubview(storeInfoView)
-//
-//        storeInfoView.addSubview(storeName)
-//        storeInfoView.addSubview(storeDescription)
+        floatingView.addSubview(floatingButton)
         
+        scrollView.addSubview(storeImageView)
+        scrollView.addSubview(storeInfoView)
+        
+        storeInfoView.addSubview(storeName)
+        storeInfoView.addSubview(storeDescription)
         
         setAutolayout()
     }
@@ -100,36 +85,31 @@ final class StoreDetailViewController: UIViewController {
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
         }
-        scrollView.addSubview(labelOne)
-
-        labelOne.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 40).isActive = true
-        labelOne.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 40).isActive = true
-
-        scrollView.addSubview(labelTwo)
-
-        labelTwo.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 40).isActive = true
-        labelTwo.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 1000).isActive = true
-        labelTwo.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -40).isActive = true
-//        storeImageView.snp.makeConstraints {
-//            $0.leading.equalToSuperview()
-//            $0.trailing.equalToSuperview()
-//            $0.top.equalToSuperview()
-//            $0.height.equalTo(600)
-//            $0.width.equalToSuperview()
-//        }
-//        storeInfoView.snp.makeConstraints {
-//            $0.leading.trailing.equalToSuperview().inset(20)
-//            $0.top.equalTo(storeImageView.snp.bottom).offset(20)
-//            $0.height.equalTo(600)
-//        }
-//        storeName.snp.makeConstraints {
-//            $0.leading.trailing.top.equalToSuperview().inset(10)
-//        }
-//        storeDescription.snp.makeConstraints {
-//            $0.top.equalTo(storeName.snp.bottom).offset(10)
-//            $0.leading.trailing.bottom.equalToSuperview().inset(10)
-//        }
+        floatingView.snp.makeConstraints { make in
+            make.left.right.bottom.equalTo(view)
+            make.height.equalTo(100)
+        }
+        floatingButton.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalToSuperview().inset(20)
+        }
+        storeImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.top.equalTo(scrollView.snp.top)
+            $0.height.equalTo(600)
+            $0.width.equalToSuperview()
+        }
+        storeInfoView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(storeImageView.snp.bottom).offset(20)
+            $0.bottom.equalTo(scrollView.snp.bottom).inset(10)
+        }
+        storeName.snp.makeConstraints {
+            $0.leading.trailing.top.equalToSuperview().inset(10)
+        }
+        storeDescription.snp.makeConstraints {
+            $0.top.equalTo(storeName.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview().inset(10)
+        }
     }
-    
-    
 }
